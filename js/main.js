668b7a6099b718ee3c2d41c1df3537be0a865a1c
Francisco Mendoza <ecoli404@icloud.com>
2022-01-09 22:26:25 -0600

    initial commit

---
+++ js/main.js
@@ -0,0 +1,45 @@
+const action1 = (element) => {
+    for(let i=0; i<7; i++) {
+        document.getElementsByClassName("side-content")[0].children[i].className = "res"
+    }
+    element.className = "active";
+};
+
+
+const team = [
+    {
+        name: "giselle maday may mukul",
+        intro: "Joven estudiante de ciencias de la comunicación del Instituto Campechano, le gusta leer y ver dramas koreanos.",
+        email: "gissmukul@gmail.com",
+        webpage: "Giselle",
+        img_URL: "./assets/profile-pictures/gis.png"
+    },
+    {
+        name: "luis che vela",
+        intro: "Joven estudiante de ciencias de la comunicación del Instituto Campechano, le gusta leer y ver dramas koreanos.",
+        email: "gissmukul@gmail.com",
+        webpage: "Giselle",
+        img_URL: "./assets/profile-pictures/lui.png"
+    },
+    {
+        name: "paola hernandez garcia",
+        intro: "Joven estudiante de ciencias de la comunicación del Instituto Campechano, le gusta leer y ver dramas koreanos.",
+        email: "gissmukul@gmail.com",
+        webpage: "Giselle",
+        img_URL: "./assets/profile-pictures/pao.png"
+    },
+    {
+        name: "marina guadalupe queb tun",
+        intro: "Joven estudiante de ciencias de la comunicación del Instituto Campechano, le gusta leer y ver dramas koreanos.",
+        email: "gissmukul@gmail.com",
+        webpage: "Giselle",
+        img_URL: "./assets/profile-pictures/mar.png"
+    },
+    {
+        name: "zelmy alvarez cahuich",
+        intro: "Joven estudiante de ciencias de la comunicación del Instituto Campechano, le gusta leer y ver dramas koreanos.",
+        email: "gissmukul@gmail.com",
+        webpage: "Giselle",
+        img_URL: "./assets/profile-pictures/zel.png"
+    }
+];
